5a32b410d6699dba3b0cc1d7387c5a4b540d487b
刘云天 <lyt112356@163.com>
2020-12-18 18:12:03 +0800

    1111

--- wxShop/pages/home/home.js
+++ wxShop/pages/home/home.js
@@ -2,16 +2,25 @@ var api = require('../../utils/api');
 Page({
   data: {
     adArr: [],
-    categoryArr:[],
-    callImgDic:{},
-    shopInfoDic:{},
-    saomaDic:{},
-    integralMallPicDic:{},
-    newUserDic:{},
-    recommendArr:[],
+    categoryArr: [],
+    callImgDic: {},
+    shopInfoDic: {},
+    saomaDic: {},
+    integralMallPicDic: {},
+    newUserDic: {},
+    recommendArr: [],
+    floor1TitleImg: {},
+    floor1ImgArr: [],
+    floor2TitleImg: {},
+    floor2ImgArr: [],
+    floor3TitleImg: {},
+    floor3ImgArr: [],
+    currentPage : 1,
+    goodsArr: [],
   },
   onLoad: function (options) {
     this.dataRequest();
+    this.goodsRequest(1);
   },
 
   dataRequest: function () {
@@ -33,10 +42,67 @@ Page({
           integralMallPicDic: res.data.data.integralMallPic,
           newUserDic: res.data.data.newUser,
           recommendArr: res.data.data.recommend,
+          floor1TitleImg: res.data.data.floor1Pic,
+          floor1ImgArr: res.data.data.floor1,
+          floor2TitleImg: res.data.data.floor2Pic,
+          floor2ImgArr: res.data.data.floor2,
+          floor3TitleImg: res.data.data.floor3Pic,
+          floor3ImgArr: res.data.data.floor3,
         })
       }
     })
   },
+
+  goodsRequest: function (page) {
+    var that = this;
+    wx.showToast({
+      title: '数据加载中',
+      icon: "loading",
+    })
+
+    if (page == 1) {
+      this.setData({
+        goodsArr: [],
+      })
+    }
+
+    api.request({
+      url: 'homePageBelowConten',
+      data: {
+        'page': page,
+      },
+      method: 'POST',
+      success: function (res) {
+        //结束下拉刷新
+        wx.stopPullDownRefresh();
+        //结束加载框
+        wx.hideToast();
+
+        that.setData({
+          goodsArr: that.data.goodsArr.concat(res.data.data),
+        })
+
+        if (res.data.data.length > 0) {
+          that.setData({
+            currentPage: page
+          })
+        }
+      },
+    })
+  },
+
+  //列表滑动到最底部
+  bindscrolltolower: function () {
+    console.log('加载更多加载更多加载更多加载更多加载更多加载更多加载更多加载更多')
+    this.goodsRequest(this.currentPage + 1);
+  },
+
+  //下拉刷新
+  onPullDownRefresh: function () {
+    this.dataRequest();
+    this.goodsRequest(1);
+  },
+
   onSwiperTap: function (event) {
     var id = event.target.dataset.id;
     console.log(id);
